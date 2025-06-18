a689c888d3daa4fe7b46a11c2685accd0c7701a7
AvivitLazra <lazraavivit@gmail.com>
2025-06-18 20:47:57 +0300

    סידור האופציות להעלאת פוסט לפתוח קבוצה והתחברות לקבוצה

--- EffiSocial/server/src/controllers/posts.js
+++ EffiSocial/server/src/controllers/posts.js
@@ -1,20 +1,20 @@
-const Post = require('../models/Post');
-const Group = require('../models/Group');
-const { validationResult } = require('express-validator');
-const User = require('../models/User');
-const asyncHandler = require('../middleware/async');
-const ErrorResponse = require('../utils/errorResponse');
-const multer = require('multer');
-const path = require('path');
+const Post = require("../models/Post");
+const Group = require("../models/Group");
+const { validationResult } = require("express-validator");
+const User = require("../models/User");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
+const multer = require("multer");
+const path = require("path");
 
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../../uploads/'));
+    cb(null, path.join(__dirname, "../../uploads/"));
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
-  }
+  },
 });
 
 const upload = multer({
@@ -23,42 +23,52 @@ const upload = multer({
   fileFilter: function (req, file, cb) {
     const filetypes = /jpeg|jpg|png|gif/;
     const mimetype = filetypes.test(file.mimetype);
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
 
     if (mimetype && extname) {
       return cb(null, true);
     }
-    cb(new Error('Only image files are allowed!'));
-  }
-}).single('media');
+    cb(new Error("Only image files are allowed!"));
+  },
+}).single("media");
 
 // @desc    Create new post
 // @route   POST /api/posts
 // @access  Private
 exports.createPost = asyncHandler(async (req, res, next) => {
+  console.log("--- Create Post Request ---");
+  console.log("User:", req.user);
+  console.log("Body before upload:", req.body);
   upload(req, res, async function (err) {
     if (err) {
+      console.error("Multer error:", err);
       return next(new ErrorResponse(err.message, 400));
     }
 
-    // Add user to req.body
-    req.body.author = req.user.id;
+    console.log("Body after upload:", req.body);
+    if (req.file) {
+      console.log("File uploaded:", req.file);
+    }
 
-    // If there's a file upload, add the file path to the post data
+    req.body.author = req.user.id;
     if (req.file) {
       req.body.media = `/uploads/${req.file.filename}`;
     }
 
-    // Create post
-    const post = await Post.create(req.body);
-
-    // Populate author details
-    await post.populate('author', 'username profilePicture');
-
-    res.status(201).json({
-      success: true,
-      data: post
-    });
+    try {
+      const post = await Post.create(req.body);
+      await post.populate("author", "username profilePicture");
+      console.log("Post created:", post);
+      res.status(201).json({
+        success: true,
+        data: post,
+      });
+    } catch (error) {
+      console.error("Error creating post:", error);
+      return next(new ErrorResponse(error.message, 500));
+    }
   });
 });
 
@@ -67,37 +77,34 @@ exports.createPost = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.getPosts = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
-  
+
   // Build query
   let query = {};
-  
+
   // If author is specified, only get posts from that author
   if (req.query.author) {
     query.author = req.query.author;
   } else {
     // Otherwise get posts from user and their friends
-    query.$or = [
-      { author: req.user.id },
-      { author: { $in: user.friends } }
-    ];
+    query.$or = [{ author: req.user.id }, { author: { $in: user.friends } }];
   }
-  
+
   // Get posts
   const posts = await Post.find(query)
-    .populate('author', 'username profilePicture')
-    .populate('likes', 'username')
+    .populate("author", "username profilePicture")
+    .populate("likes", "username")
     .populate({
-      path: 'comments',
+      path: "comments",
       populate: {
-        path: 'author',
-        select: 'username profilePicture'
-      }
+        path: "author",
+        select: "username profilePicture",
+      },
     })
-    .sort('-createdAt');
+    .sort("-createdAt");
 
   res.status(200).json({
     success: true,
-    data: posts
+    data: posts,
   });
 });
 
@@ -107,26 +114,26 @@ exports.getPosts = asyncHandler(async (req, res, next) => {
 exports.getPost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
-      .populate('author', 'username profilePicture')
-      .populate('group', 'name')
-      .populate('comments.author', 'username profilePicture');
+      .populate("author", "username profilePicture")
+      .populate("group", "name")
+      .populate("comments.author", "username profilePicture");
 
     if (!post) {
       return res.status(404).json({
         success: false,
-        message: 'Post not found'
+        message: "Post not found",
       });
     }
 
     res.status(200).json({
       success: true,
-      data: post
+      data: post,
     });
   } catch (err) {
     console.error(err);
     res.status(500).json({
       success: false,
-      message: 'Server error'
+      message: "Server error",
     });
   }
 };
@@ -144,7 +151,7 @@ exports.updatePost = asyncHandler(async (req, res, next) => {
   }
 
   // Make sure user is post owner
-  if (post.author.toString() !== req.user.id && req.user.role !== 'admin') {
+  if (post.author.toString() !== req.user.id && req.user.role !== "admin") {
     return next(
       new ErrorResponse(
         `User ${req.user.id} is not authorized to update this post`,
@@ -155,12 +162,12 @@ exports.updatePost = asyncHandler(async (req, res, next) => {
 
   post = await Post.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
-    runValidators: true
-  }).populate('author', 'username profilePicture');
+    runValidators: true,
+  }).populate("author", "username profilePicture");
 
   res.status(200).json({
     success: true,
-    data: post
+    data: post,
   });
 });
 
@@ -177,7 +184,7 @@ exports.deletePost = asyncHandler(async (req, res, next) => {
   }
 
   // Make sure user is post owner
-  if (post.author.toString() !== req.user.id && req.user.role !== 'admin') {
+  if (post.author.toString() !== req.user.id && req.user.role !== "admin") {
     return next(
       new ErrorResponse(
         `User ${req.user.id} is not authorized to delete this post`,
@@ -190,7 +197,7 @@ exports.deletePost = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
-    data: {}
+    data: {},
   });
 });
 
@@ -221,7 +228,7 @@ exports.likePost = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
-    data: post
+    data: post,
   });
 });
 
@@ -239,7 +246,7 @@ exports.addComment = asyncHandler(async (req, res, next) => {
 
   const newComment = {
     content: req.body.content,
-    author: req.user.id
+    author: req.user.id,
   };
 
   post.comments.unshift(newComment);
@@ -248,16 +255,16 @@ exports.addComment = asyncHandler(async (req, res, next) => {
 
   // Populate the new comment's author details
   await post.populate({
-    path: 'comments',
+    path: "comments",
     populate: {
-      path: 'author',
-      select: 'username profilePicture'
-    }
+      path: "author",
+      select: "username profilePicture",
+    },
   });
 
   res.status(200).json({
     success: true,
-    data: post
+    data: post,
   });
 });
 
@@ -275,18 +282,21 @@ exports.removeComment = asyncHandler(async (req, res, next) => {
 
   // Pull out comment
   const comment = post.comments.find(
-    comment => comment._id.toString() === req.params.comment_id
+    (comment) => comment._id.toString() === req.params.comment_id
   );
 
   // Make sure comment exists
   if (!comment) {
     return next(
-      new ErrorResponse(`Comment not found with id of ${req.params.comment_id}`, 404)
+      new ErrorResponse(
+        `Comment not found with id of ${req.params.comment_id}`,
+        404
+      )
     );
   }
 
   // Make sure user is comment owner
-  if (comment.author.toString() !== req.user.id && req.user.role !== 'admin') {
+  if (comment.author.toString() !== req.user.id && req.user.role !== "admin") {
     return next(
       new ErrorResponse(
         `User ${req.user.id} is not authorized to delete this comment`,
@@ -297,7 +307,7 @@ exports.removeComment = asyncHandler(async (req, res, next) => {
 
   // Get remove index
   const removeIndex = post.comments
-    .map(comment => comment._id.toString())
+    .map((comment) => comment._id.toString())
     .indexOf(req.params.comment_id);
 
   post.comments.splice(removeIndex, 1);
@@ -306,6 +316,6 @@ exports.removeComment = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
-    data: post
+    data: post,
   });
-}); 
+});
